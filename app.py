e9d3cd9121a82ed277db4f584c0a769c378157cb
AAKASH GORADIA <aakash7goradia@gmail.com>
2024-04-04 05:44:56 +0530

    form

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hello world"
+def portfolio():
+    return render_template('index.html')
 
+@app.route('/contact/',methods=['GET','POST'])
+def contact():
+    if request.method=='POST':
+        name=request.form['name']
+        subject=request.form['Subject']
+        email=request.form['_replyto']
+        message=request.form['message']
+        
+    return render_template('contact.html',name=name, subject=subject, email=email, message=message)
+                
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
